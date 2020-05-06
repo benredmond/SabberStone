22b7c8370e870c47cd31416f5d702d3fb669ec59
benredmond <bjr0206@gmail.com>
2020-05-06 14:32:35 -0500

    initial commit

---
+++ core-extensions/SabberStoneCoreAi/src/Agent/TestAgent.cs
@@ -0,0 +1,219 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SabberStoneCore.Tasks;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.POGame;
+using SabberStoneCore.Tasks.PlayerTasks;
+using System.Linq;
+
+
+namespace SabberStoneCoreAi.Agent
+{
+    class TestAgent : AbstractAgent
+    {
+        public static string HERO_HEALTH_REDUCED = "HERO_HEALTH_REDUCED";
+        public static string HERO_ATTACK_REDUCED = "HERO_ATTACK_REDUCED";
+        public static string WEAPON_DURABILITY_REDUCED = "WEAPON_DURABILITY_REDUCED";
+        public static string MINION_HEALTH_REDUCED = "MINION_HEALTH_REDUCED";
+        public static string MINION_ATTACK_REDUCED = "MINION_ATTACK_REDUCED";
+        public static string MINION_KILLED = "MINION_KILLED";
+        public static string MINION_APPEARED = "MINION_APPEARED";
+        public static string SECRET_REMOVED = "SECRET_REMOVED";
+        public static string MANA_REDUCED = "MANA_REDUCED";
+        public static string M_HEALTH = "M_HEALTH";
+        public static string M_ATTACK = "M_ATTACK";
+        public static string M_HAS_CHARGE = "M_HAS_CHARGE";
+        public static string M_HAS_DEAHTRATTLE = "M_HAS_DEAHTRATTLE";
+        public static string M_HAS_DIVINE_SHIELD = "M_HAS_DIVINE_SHIELD";
+        public static string M_HAS_LIFE_STEAL = "M_HAS_DIVINE_SHIELD";
+        public static string M_HAS_STEALTH = "M_HAS_STEALTH";
+        public static string M_HAS_TAUNT = "M_HAS_TAUNT";
+        public static string M_HAS_WINDFURY = "M_HAS_WINDFURY";
+        public static string M_IS_RUSH = "M_IS_RUSH";
+        public static string M_MANA_COST = "M_MANA_COST";
+        public static string M_POISONOUS = "M_POISONOUS";	
+        public static string M_SILENCED = "M_SILENCED";
+        public static string M_SUMMONED = "M_SUMMONED";
+        public static string M_CANT_BE_TARGETED_BY_SPELLS = "M_CANT_BE_TARGETED_BY_SPELLS";
+		public Dictionary<string, double> weights = new Dictionary<string, double>();
+
+        public TestAgent(List<double> initWeights)
+        {
+            weights[HERO_HEALTH_REDUCED] = initWeights[0];
+            weights[HERO_ATTACK_REDUCED] = initWeights[1];
+            weights[WEAPON_DURABILITY_REDUCED] = initWeights[2];
+            weights[MINION_HEALTH_REDUCED] = initWeights[3];
+            weights[MINION_ATTACK_REDUCED] = initWeights[4];
+            weights[MINION_KILLED] = initWeights[5];
+            weights[MINION_APPEARED] = initWeights[6];
+            weights[SECRET_REMOVED] = initWeights[7];
+            weights[MANA_REDUCED] = initWeights[8];
+            weights[M_HEALTH] = initWeights[9];
+            weights[M_ATTACK] = initWeights[10];
+            weights[M_HAS_CHARGE] = initWeights[11];
+            weights[M_HAS_DEAHTRATTLE] = initWeights[12];
+            weights[M_HAS_DIVINE_SHIELD] = initWeights[13];
+            weights[M_HAS_LIFE_STEAL] = initWeights[14];
+            weights[M_HAS_STEALTH] = initWeights[15];
+            weights[M_HAS_TAUNT] = initWeights[16];
+            weights[M_HAS_WINDFURY] = initWeights[17];
+            weights[M_IS_RUSH] = initWeights[18];
+            weights[M_MANA_COST] = initWeights[19];
+            weights[M_POISONOUS] = initWeights[20];
+            weights[M_SILENCED] = initWeights[21];
+            weights[M_SUMMONED] = initWeights[22];
+            weights[M_CANT_BE_TARGETED_BY_SPELLS] = initWeights[23];
+        }
+
+		public override void FinalizeAgent()
+		{
+		}
+
+		public override void FinalizeGame()
+		{
+		}
+
+		public override PlayerTask GetMove(SabberStoneCoreAi.POGame.POGame poGame)
+		{
+            return getBestTask(poGame);
+		}
+
+        public void initWeights() 
+        {
+            weights[HERO_HEALTH_REDUCED] = 1;
+            weights[HERO_ATTACK_REDUCED] = 1;
+            weights[WEAPON_DURABILITY_REDUCED] = 1;
+            weights[MINION_HEALTH_REDUCED] = 1;
+            weights[MINION_ATTACK_REDUCED] = 1;
+            weights[MINION_KILLED] = 1;
+            weights[MINION_APPEARED] = 1;
+            weights[SECRET_REMOVED] = 1;
+            weights[MANA_REDUCED] = 1;
+            weights[M_HEALTH] = 1;
+            weights[M_ATTACK] = 1;
+            weights[M_SUMMONED] = 1;
+            weights[M_HAS_CHARGE] = 1;
+            weights[M_HAS_DEAHTRATTLE] = 1;
+            weights[M_HAS_DIVINE_SHIELD] = 1;
+            weights[M_HAS_LIFE_STEAL] = 1;
+            weights[M_HAS_STEALTH] = 1;
+            weights[M_HAS_TAUNT] = 1;
+            weights[M_HAS_WINDFURY] = 1;
+            weights[M_IS_RUSH] = 1;
+            weights[M_MANA_COST] = 1;
+            weights[M_POISONOUS] = 1;
+            // switch (poGame.)
+        } 
+
+        public PlayerTask getBestTask(SabberStoneCoreAi.POGame.POGame poGame) {
+            double bestScore = Double.MinValue;
+            PlayerTask bestTask = SabberStoneCore.Tasks.PlayerTasks.EndTurnTask.Any(poGame.CurrentPlayer);
+            List<PlayerTask> tasksToSimulate = poGame.CurrentPlayer.Options();
+            Dictionary<PlayerTask, SabberStoneCoreAi.POGame.POGame> simulation = poGame.Simulate(tasksToSimulate);
+            
+            foreach (PlayerTask task in tasksToSimulate) {
+                double score = scoreTask(poGame, simulation[task]);
+                if (task.PlayerTaskType == PlayerTaskType.END_TURN)
+				{
+					score = 0;
+				}
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestTask = task;
+                }
+            }
+
+            return bestTask;
+        }
+
+        public double scoreTask(SabberStoneCoreAi.POGame.POGame currentState, SabberStoneCoreAi.POGame.POGame nextState) {
+            if (nextState == null) return Double.MinValue;
+            if (nextState.CurrentOpponent.Hero.Health <= 0) {
+                return Double.MaxValue;
+            }
+
+            if (nextState.CurrentPlayer.Hero.Health <= 0) {
+                return Double.MinValue;
+            }
+
+            double enemyHeroScore = scoreHero(currentState.CurrentOpponent, nextState.CurrentOpponent);
+            double playerHeroScore = scoreHero(currentState.CurrentPlayer, nextState.CurrentPlayer);
+            double enemyMinionsScore = scoreMinions(currentState.CurrentOpponent.BoardZone, nextState.CurrentOpponent.BoardZone);
+            double playerMinionsScore = scoreMinions(currentState.CurrentPlayer.BoardZone, nextState.CurrentPlayer.BoardZone);
+            double enemySecretsScore = scoreSecrets(currentState.CurrentOpponent, nextState.CurrentOpponent);
+            double playerSecretsScore = scoreSecrets(currentState.CurrentPlayer, nextState.CurrentPlayer);
+            double manaUsedScore = weights[MANA_REDUCED] * (currentState.CurrentPlayer.RemainingMana - nextState.CurrentPlayer.RemainingMana);
+            
+            return enemyHeroScore - playerHeroScore + enemyMinionsScore - playerMinionsScore + enemySecretsScore - playerSecretsScore - manaUsedScore;
+        }
+
+        public double scoreHero(SabberStoneCore.Model.Entities.Controller currentHero, SabberStoneCore.Model.Entities.Controller nextHero) {
+            int healthDiff = (currentHero.Hero.Health + currentHero.Hero.Armor) - (nextHero.Hero.Health + nextHero.Hero.Armor);
+            int attackDiff = currentHero.Hero.TotalAttackDamage - nextHero.Hero.TotalAttackDamage;
+            int weaponDiff = (currentHero.Hero.Weapon!= null ? currentHero.Hero.Weapon.Durability : 0) - (nextHero.Hero.Weapon != null ? nextHero.Hero.Weapon.Durability : 0);
+            return healthDiff * weights[HERO_HEALTH_REDUCED] + attackDiff * weights[HERO_ATTACK_REDUCED] + weaponDiff * weights[WEAPON_DURABILITY_REDUCED];
+        }
+
+        public double scoreMinions(SabberStoneCore.Model.Zones.BoardZone currentBoard, SabberStoneCore.Model.Zones.BoardZone nextBoard) {
+            double scoreHealthReduced = 0;
+			double scoreAttackReduced = 0;
+            double scoreSilenced = 0;
+			double scoreKilled = 0;
+			double scoreSummoned = 0;
+            //Add frozen, divine shield
+
+            foreach (SabberStoneCore.Model.Entities.Minion currentMinion in currentBoard.GetAll()) {
+                bool minionSurvived = false;
+
+                foreach (SabberStoneCore.Model.Entities.Minion nextMinion in nextBoard.GetAll(minion => minion.Id == currentMinion.Id)) {
+                    scoreHealthReduced += weights[MINION_HEALTH_REDUCED] * (currentMinion.Health - nextMinion.Health) * scoreMinion(currentMinion);
+                    scoreAttackReduced += weights[MINION_ATTACK_REDUCED] * (currentMinion.AttackDamage - nextMinion.AttackDamage) * scoreMinion(currentMinion);
+                    scoreSilenced += !currentMinion.IsSilenced && nextMinion.IsSilenced ? weights[M_SILENCED] * scoreMinion(currentMinion) : 0;
+                    minionSurvived = true;
+                }
+
+                if (!minionSurvived) {
+                    scoreKilled += weights[MINION_KILLED] * scoreMinion(currentMinion);
+                }
+            }
+
+            foreach (SabberStoneCore.Model.Entities.Minion minion in nextBoard.GetAll().Except(currentBoard.GetAll())) {
+                scoreSummoned += weights[M_SUMMONED] * scoreMinion(minion);
+            }
+
+            return scoreHealthReduced + scoreAttackReduced + scoreSilenced + scoreKilled - scoreSummoned;
+        }
+
+        public double scoreMinion(SabberStoneCore.Model.Entities.Minion minion) {
+            double minionScore = 0;
+
+            if (minion.HasCharge) minionScore += weights[M_HAS_CHARGE];
+            if (minion.IsRush) minionScore += weights[M_IS_RUSH];
+            if (minion.HasDeathrattle) minionScore += weights[M_HAS_DEAHTRATTLE];
+            if (minion.HasDivineShield) minionScore += weights[M_HAS_DIVINE_SHIELD];
+            if (minion.HasLifeSteal) minionScore += weights[M_HAS_LIFE_STEAL];
+            if (minion.HasTaunt) minionScore += weights[M_HAS_TAUNT];
+            if (minion.HasWindfury) minionScore += weights[M_HAS_WINDFURY];
+            if (minion.HasStealth) minionScore += weights[M_HAS_STEALTH];
+            if (minion.CantBeTargetedBySpells) minionScore += weights[M_CANT_BE_TARGETED_BY_SPELLS];
+            if (minion.Poisonous) minionScore += weights[M_POISONOUS];
+            minionScore += weights[M_MANA_COST] * minion.Card.Cost;
+            return minionScore;
+        }
+
+        public double scoreSecrets(SabberStoneCore.Model.Entities.Controller currentHero, SabberStoneCore.Model.Entities.Controller nextHero) {
+            return weights[SECRET_REMOVED] * (currentHero.SecretZone.Count - nextHero.SecretZone.Count);
+        }
+
+		public override void InitializeAgent()
+		{
+            // initWeights();
+		}
+
+		public override void InitializeGame()
+		{
+		}
+    }
+}
---
+++ core-extensions/SabberStoneCoreAi/src/Program.cs
@@ -0,0 +1,168 @@
+﻿#region copyright
+// SabberStone, Hearthstone Simulator in C# .NET Core
+// Copyright (C) 2017-2019 SabberStone Team, darkfriend77 & rnilva
+//
+// SabberStone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
+// SabberStone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using SabberStoneCore.Config;
+using SabberStoneCore.Enums;
+using SabberStoneCoreAi.POGame;
+using SabberStoneCoreAi.Agent.ExampleAgents;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.Meta;
+using SabberStoneCore.Model;
+using System.IO;
+using System.Text;
+
+namespace SabberStoneCoreAi
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+
+			Dictionary<string, CardClass> classMap = new Dictionary<string, CardClass>();
+			Dictionary<string, List<Card>> deckMap = new Dictionary<string, List<Card>>();
+
+			classMap.Add("RenoKazakusMage", CardClass.MAGE);
+			classMap.Add("MiraclePirateRogue", CardClass.ROGUE);
+			classMap.Add("ZooDiscardWarlock", CardClass.WARLOCK);
+			classMap.Add("RenoKazakusDragonPriest", CardClass.PRIEST);
+			classMap.Add("MidrangeBuffPaladin", CardClass.PALADIN);
+			// classMap.Add("MurlocDruid", CardClass.DRUID);
+			classMap.Add("MidrangeJadeShaman", CardClass.SHAMAN);
+			classMap.Add("AggroPirateWarrior", CardClass.WARRIOR);
+
+			deckMap.Add("RenoKazakusMage", Decks.RenoKazakusMage);
+			deckMap.Add("MiraclePirateRogue", Decks.MiraclePirateRogue);
+			deckMap.Add("ZooDiscardWarlock", Decks.ZooDiscardWarlock);
+			deckMap.Add("RenoKazakusDragonPriest", Decks.RenoKazakusDragonPriest);
+			deckMap.Add("MidrangeBuffPaladin", Decks.MidrangeBuffPaladin);
+			// deckMap.Add("MurlocDruid", Decks.MurlocDruid);
+			deckMap.Add("MidrangeJadeShaman", Decks.MidrangeJadeShaman);
+			deckMap.Add("AggroPirateWarrior", Decks.AggroPirateWarrior);
+
+			List<string> deckList = new List<String>(deckMap.Keys);
+
+			bool genData = false;
+			if (genData) {
+				runSimulation(classMap, deckMap, deckList);
+			} else {
+				List<Double> weights = new List<double>{0.02978996187448501587,-0.00318665639497339725,0.00348940002731978893,-0.00400655437260866165,0.04111846536397933960,0.11956916004419326782,-0.01585331372916698456,0.01084957178682088852,-0.12021212279796600342,-0.00146640429738909006,0.00578224333003163338,0.00287493830546736717,0.02130169607698917389,-0.00203602435067296028,0.01017116382718086243,-0.00217816513031721115,0.01499202288687229156,0.01702820882201194763,-0.00332036474719643593,0.42425787448883056641,0.00005312222856446169,0.00092798698460683227,0.46059826016426086426,-0.00093383796047419310};
+				testWeights(classMap, deckMap, deckList, weights);
+			}
+			
+		}
+
+		private static void testWeights(Dictionary<string, CardClass> classMap, Dictionary<string, List<Card>> deckMap, List<string> deckList, List<double> weights) {
+			Console.WriteLine("Simulate Games");
+			Random r = new Random();	
+			double wins = 0.0;
+			double losses = 0.0;
+			double numGames = 3000;
+
+			for (int i = 0; i < numGames; i++) {
+	
+				string p1Deck = deckList[r.Next(deckList.Count)];
+				string p2Deck = deckList[r.Next(deckList.Count)];
+
+				var gameConfig = new GameConfig()
+				{
+					StartPlayer = r.Next(2) + 1,
+					Player1HeroClass = classMap.GetValueOrDefault(p1Deck, CardClass.MAGE),
+					Player2HeroClass = classMap.GetValueOrDefault(p2Deck, CardClass.ROGUE),
+					Player1Deck = deckMap.GetValueOrDefault(p1Deck, Decks.RenoKazakusMage),
+					Player2Deck = deckMap.GetValueOrDefault(p2Deck, Decks.MiraclePirateRogue),
+					FillDecks = true,
+					Shuffle = true,
+					Logging = false
+				};
+
+				AbstractAgent player1 = new GreedyAgent();
+				AbstractAgent player2 = new TestAgent(weights);
+				Console.WriteLine("Game " + i + " " + p1Deck + " " + p2Deck);		
+				var gameHandler = new POGameHandler(gameConfig, player1, player2, repeatDraws:false);
+				gameHandler.PlayGames(nr_of_games:1, addResultToGameStats:true, debug:false);
+				GameStats gameStats = gameHandler.getGameStats();
+				wins += gameStats.PlayerB_Wins;
+				losses += gameStats.PlayerA_Wins;
+			}
+
+			Console.WriteLine("Bot winrate: " + (wins/(wins + losses)));
+			Console.WriteLine("Wins: " + wins);
+			Console.WriteLine("Losses: " + losses);
+			Console.WriteLine("Draws: " + (numGames - (wins + losses)));
+		}
+
+		private static void runSimulation(Dictionary<string, CardClass> classMap, Dictionary<string, List<Card>> deckMap, List<string> deckList) {
+			int numGames = 3000;
+			Console.WriteLine("Simulate Games");
+
+			List<List<double>> games = new List<List<double>>();
+			for (int i = 0; i < numGames; i++) {
+				Random r = new Random();
+				double max = 10;
+				double min = -10;
+				List<double> randWeights = 
+					Enumerable.Range(0, 24) // create sequence of 100 elements
+					.Select(_ =>r.NextDouble() * (max - min) + min) // for each element select random value
+					.ToList(); // convert to array.
+
+				string p1Deck = deckList[r.Next(deckList.Count)];
+				string p2Deck = deckList[r.Next(deckList.Count)];
+
+				var gameConfig = new GameConfig()
+				{
+					StartPlayer = 1,
+					Player1HeroClass = classMap.GetValueOrDefault(p1Deck, CardClass.MAGE),
+					Player2HeroClass = classMap.GetValueOrDefault(p2Deck, CardClass.ROGUE),
+					Player1Deck = deckMap.GetValueOrDefault(p1Deck, Decks.RenoKazakusMage),
+					Player2Deck = deckMap.GetValueOrDefault(p2Deck, Decks.MiraclePirateRogue),
+					FillDecks = true,
+					Shuffle = true,
+					Logging = false
+				};
+
+				AbstractAgent player1 = new RandomAgent();
+				AbstractAgent player2 = new TestAgent(randWeights);
+				Console.WriteLine("Game " + i + " " + p1Deck + " " + p2Deck);		
+				var gameHandler = new POGameHandler(gameConfig, player1, player2, repeatDraws:false);
+				gameHandler.PlayGames(nr_of_games:1, addResultToGameStats:true, debug:false);
+				GameStats gameStats = gameHandler.getGameStats();
+				// gameStats.printResults();
+				randWeights.Add(gameStats.PlayerB_Wins);
+				games.Add(randWeights);
+				// Console.WriteLine(String.Join(",", randWeights.Select(p=>p.ToString()).ToArray()));
+			}
+
+			StringBuilder sb = new StringBuilder();
+			if(!File.Exists(@"test.csv")) {
+				sb.Append("HERO_HEALTH_REDUCED,HERO_ATTACK_REDUCED,WEAPON_DURABILITY_REDUCED,MINION_HEALTH_REDUCED,MINION_ATTACK_REDUCED,MINION_KILLED,MINION_APPEARED,SECRET_REMOVED,MANA_REDUCED,M_HEALTH,M_ATTACK,M_HAS_CHARGE,M_HAS_DEAHTRATTLE,M_HAS_DIVINE_SHIELD,M_HAS_LIFE_STEAL,M_HAS_STEALTH,M_HAS_TAUNT,M_HAS_WINDFURY,M_IS_RUSH,M_MANA_COST,M_POISONOUS,M_SILENCED,M_SUMMONED,M_CANT_BE_TARGETED_BY_SPELLS,RESULT\r\n");
+			}
+
+			for (int i = 0; i < games.Count(); i++)
+			{
+				for (int j = 0; j < games[0].Count(); j++)
+				{
+					sb.Append((j==0 ? "" : ",") + games[i][j]);
+				}
+				sb.AppendLine();
+			}
+			
+			File.AppendAllText(@"test.csv", sb.ToString());
+			Console.WriteLine("Test successful");
+			Console.ReadLine();
+		}
+	}
+}
